b38e63f47a6f0343a374d0d0fb7727a592aef4d2
Naian Koval <kovalnayan666@gmail.com>
2026-05-10 22:56:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
-int main() {return 0;}
+#include <iostream>
+
+int main() {
+    // Виведення тексту на екран
+    std::cout << "Hello, World!" << std::endl;
+    return 0; // Повернення успішного коду
+}
+
